850244b8004c35ac2d382c79503376b30cbf32b3
Gyozo Kudor <kudorgyozo@gmail.com>
2025-01-27 17:29:32 +0200

    standalone components, removed modules tailwind shitfuckery signals, zoneless

--- rentathing.client/src/app/app.component.ts
+++ rentathing.client/src/app/app.component.ts
@@ -1,38 +1,38 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
 
 interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+    date: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
+    public forecasts = signal<WeatherForecast[]>([]);
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-    this.getForecasts();
-  }
+    ngOnInit() {
+        this.getForecasts();
+    }
 
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
+    getForecasts() {
+        this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
+            (result) => {
+                this.forecasts.set(result);
+            },
+            (error) => {
+                console.error(error);
+            }
+        );
+    }
 
-  title = 'RentAThing.Client';
+    title = 'RentAThing.Client';
 }
--- rentathing.client/src/main.ts
+++ rentathing.client/src/main.ts
@@ -1,7 +1,17 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
+import { bootstrapApplication } from '@angular/platform-browser';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { routes } from './app/routes';
+import { AppComponent } from './app/app.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideExperimentalZonelessChangeDetection(),
+        provideRouter(routes),
+        //importProvidersFrom(BrowserModule, AppRoutingModule),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
 })
-  .catch(err => console.error(err));
+    .catch(err => console.error(err));
